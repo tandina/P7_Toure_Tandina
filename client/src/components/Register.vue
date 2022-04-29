e92e7217983fd234e90298619cd5912adabc5980
tandina <touretmc@protonmail.com>
2022-04-29 20:34:58 +0200

    installation des dépendances et création structure

---
+++ client/src/components/Register.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <h1>Login</h1>
+
+    <input
+    type="email"
+    name="email"
+    v-model="email"
+    placeholder="email" />
+    <br>
+    <input
+    type="password"
+    name="password"
+    v-model="password"
+    placeholder="enter password" />
+    <br>
+    <button
+      @click="register">Login</button>
+  </div>
+</template>
+
+<script>
+import AuthenticationService from '@/services/AuthenticationService'
+export default {
+  name: 'Register',
+  data () {
+    return {
+      email: '',
+      password: ''
+    }
+  },
+  methods: {
+    async register () {
+      await AuthenticationService.register({
+        email: this.email,
+        password: this.password
+      })
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
